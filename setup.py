c4df968acf50c3a4ad6f6b111b0106a34e16e69c
Paulina Konet <paulina.konet@gmail.com>
2019-07-08 12:23:11 +0800

    fixed setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,17 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-
-from setuptools import setup, find_packages # type: ignore
-from os import path
+from setuptools import setup, find_packages  # type: ignore
 
 setup(
-    name="WFSLib",
-    version="1.0",
-    package_data={'': ['*.*']},
-    packages=find_packages(),
+    name='wfslib',
+    description='Tool for processing WFS data',
+    # long_description=long_description,
+    # version=wfslib.__version__,
+    version='1.0',
+    author="Zoya Volovikova, Polina Konetskaya",
+    license=license,
+    classifiers=[
+        'Programming Language :: Python :: 3.7',
+    ],
+    keywords='wfs',
+    packages=find_packages(exclude=['docs', 'tests']),
+    python_requires='>=3.7',
 )

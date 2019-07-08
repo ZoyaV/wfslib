3639ebbfcd3184cd97a1efe7a178b3518c17bfbc
Paulina Konet <paulina.konet@gmail.com>
2019-07-08 12:23:59 +0800

    fixed conftest.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,11 +1,11 @@
 import h5py  # type: ignore
 
-import pytest # type: ignore
-import numpy as np # type: ignore
+import pytest  # type: ignore
+import numpy as np  # type: ignore
 
 
 @pytest.fixture(scope="function")
 def load_examples() -> np.lib.npyio.NpzFile:
-    data = np.load("images.npz")
+    data = np.load('data/images.npz')
     print(type(data))
     return data
